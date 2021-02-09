f4c748e5805e4e06913cf0da4f54cf0cefdce41d
Your Name <you@example.com>
2021-02-09 22:06:24 +0800

    nitial commit

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = ' ';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成抚州禁毒';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
